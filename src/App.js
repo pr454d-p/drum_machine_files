a3bba90dd5fc4ea0e05740285011e9f5bd768f44
pr454d-p <7697prasad@gmail.com>
2022-09-22 07:29:07 +0530

    final draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState , useEffect } from 'react';
+import './style.css';
+import cev from './music/Cev_H2.mp3';
+import dsc from "./music/Dsc_Oh.mp3";
+import hea1 from './music/Heater-1.mp3';
+import hea2 from './music/Heater-2.mp3';
+import hea3 from './music/Heater-3.mp3';
+import hea4 from './music/Heater-4_1.mp3';
+import hea6 from './music/Heater-6.mp3';
+import kick from './music/Kick_n_Hat.mp3';
+import rp4 from './music/RP4_KICK_1.mp3';
 
 function App() {
+  const [displayScreen, setDisplayScreen] = useState('')
+  useEffect(()=>{
+    document.addEventListener('keydown',(event)=>[
+      playMusic(event.key.toUpperCase())
+    ])
+  },[]);
+  const drumSound = [
+    {
+      id:113,
+      text:'Q',
+      src: cev
+    },
+    {
+      id:119,
+      text:'W',
+      src: dsc
+    },
+    {
+      id:101,
+      text:'E',
+      src: hea1
+    },
+    {
+      id:97,
+      text:'A',
+      src: hea2
+    },
+    {
+      id:115,
+      text:'S',
+      src: hea3
+    },
+    {
+      id:100,
+      text:'D',
+      src: hea4
+    },
+    {
+      id:122,
+      text:'Z',
+      src: hea6
+    },
+    {
+      id:120,
+      text:'X',
+      src: kick
+    },
+    {
+      id:99,
+      text:'C',
+      src: rp4
+    }
+  ]
+const playMusic =(beat)=>{
+const audio = document.getElementById(beat)
+audio.play();
+setDisplayScreen(beat)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="drum-machine" >
+        {
+          drumSound.map((drumpad)=> 
+          <button key={drumpad.src} 
+          className='drum-pad' id={drumpad.src} onClick={()=>{
+            playMusic(drumpad.text)
+          }} >
+            {drumpad.text}
+            <audio className='clip' id={drumpad.text} src={drumpad.src}>
+            </audio>
+          </button>)
+        }
+      <div id="display">
+        {displayScreen}
+      </div>
+
+      </div>
     </div>
   );
 }
